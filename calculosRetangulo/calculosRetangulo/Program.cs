530addd448d2f1380c473599c1a52a9c9467060f
tramos <70649355+tatimramos@users.noreply.github.com>
2024-02-10 19:11:24 -0300

    Add files via upload

---
+++ calculosRetangulo/calculosRetangulo/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using calculosRetangulo;
+using System.Globalization;
+
+/*Fazer um programa para ler os valores da largura e altura de um retangulo e 
+ * calcular o valor de sua área, perímetro e diagonal.*/
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Retangulo t = new Retangulo();
+
+        Console.WriteLine("Entre a largura e altura do retangulo:");
+        t.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        t.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine(t);
+    }
+}
---
+++ calculosRetangulo/calculosRetangulo/Retangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculosRetangulo
+{
+    public class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+
+
+        public override string ToString()
+        {
+            return "AREA = "
+                + Area().ToString("F2", CultureInfo.InvariantCulture)
+                + "\nPERIMETRO = "
+                + Perimetro().ToString("F2", CultureInfo.InvariantCulture)
+                + "\nDIAGONAL = "
+                + Diagonal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double Diagonal()
+        {
+            double diag = Math.Pow(Largura, 2) + Math.Pow(Altura, 2);
+            return Math.Sqrt(diag);
+        }
+    }
+}
